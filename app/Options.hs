9aa9a21d359f79d09d11f6e197f5b391686acf42
Benedikt Rips <benedikt.rips@gmail.com>
2026-03-10 15:42:01 +0100

    feat(options): show default values

--- app/Options.hs
+++ app/Options.hs
@@ -6,24 +6,7 @@ module Options (
   optionsInfo,
 ) where
 
-import Options.Applicative (
-  Parser,
-  ParserInfo,
-  argument,
-  eitherReader,
-  fullDesc,
-  header,
-  help,
-  helper,
-  info,
-  long,
-  metavar,
-  option,
-  progDesc,
-  short,
-  switch,
-  value,
- )
+import Options.Applicative
 import Text.Megaparsec (parseMaybe)
 
 import CNF
@@ -40,53 +23,52 @@ data Options = Options
 -- The output format.
 data OutputFormat = Text | Latex
 
--- The options parser.
+-- The option parser.
 options :: Parser Options
 options =
   Options
-    <$> _formula
-    <*> _listSteps
-    <*> _outputFormat
-    <*> _showSolution
+    <$> formula
+    <*> listSteps
+    <*> outputFormat
+    <*> showSolution
  where
-  _formula =
-    argument parseFormula $
-      metavar "FORMULA"
+  formula = argument parseFormula (metavar "FORMULA")
    where
     parseFormula = eitherReader $
       \s -> case parseMaybe Parser.formula s of
         Nothing -> Left "parse error."
         Just f -> Right f
-  _listSteps =
+  listSteps =
     switch $
       long "list-steps"
         <> short 'l'
         <> help "List the steps of the DPLL algorithm execution."
-  _outputFormat =
+  outputFormat =
     option parseOutputFormat $
       long "output-format"
         <> short 'o'
         <> metavar "FORMAT"
         <> value Text
-        <> help
-          ( "The output format.  Currently supported: text and latex.  "
-              ++ "Defaults to text."
-          )
+        <> showDefaultWith showOutputFormat
+        <> help "The output format. Currently supported: text and latex. Defaults to text."
    where
     parseOutputFormat = eitherReader $ \case
       "text" -> Right Text
       "latex" -> Right Latex
       fmt -> Left $ "unknown output format: " ++ fmt ++ "."
-  _showSolution =
+    showOutputFormat = \case
+      Text -> "text"
+      Latex -> "latex"
+  showSolution =
     switch $
       long "show-solution"
         <> short 's'
         <> help "Show a solution to the formula, if satisfiable."
 
--- The options parser with the global program info and description.
+-- The option parser with the global program info and description.
 optionsInfo :: ParserInfo Options
 optionsInfo =
   info (helper <*> options) $
     fullDesc
-      <> progDesc "Determine the satisfiability of the FORMULA."
-      <> header "dpll - a SAT solver"
+      <> progDesc "Determine the satisfiability of Boolean formulae."
+      <> header "sat — a SAT solver"
